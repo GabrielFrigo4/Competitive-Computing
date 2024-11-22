6c0bf60da1aeb3a31bcccfec62be8495a9bb4187
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-11-21 21:19:51 -0300

    Update CodeForces

---
+++ Treino/2) CodeForces/Div 3/Round 988/E/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <numeric>
+#include <cstdint>
+#include <cstddef>
+#include <climits>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+
+void test_case(int64_t t) {
+	return;
+}
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(nullptr);
+	std::cin.tie(nullptr);
+
+	int64_t t;
+	while (std::cin >> t) {
+		test_case(t);
+	}
+	return 0;
+}
---
+++ Treino/2) CodeForces/Div 3/Round 988/F/main.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <numeric>
+#include <cstdint>
+#include <cstddef>
+#include <climits>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+
+void test_case(void) {
+	return;
+}
+
+int main(void) {
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(nullptr);
+	std::cin.tie(nullptr);
+
+	int64_t t;
+	std::cin >> t;
+
+	while (t-- > 0) {
+		test_case();
+	}
+	return 0;
+}
