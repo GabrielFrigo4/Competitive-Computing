de7a9e2203b5ccb8c6da79e6a3ce3dfc3675d26a
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-11-22 14:11:07 -0300

    Update Layers

--- Competição/Layers/Altas Aventuras/main.py
+++ Competição/Layers/Altas Aventuras/main.py
@@ -6,18 +6,18 @@ k = int(l[1])
 output = 0
 
 while (k > 2 and n > 0):
-    k -= 1
-    output += 1
-    n = n // 2
+	k -= 1
+	output += 1
+	n = n // 2
 
 if k == 2:
-    a = 1
-    b = 1
-    c = -2*n
-    x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-    x = math.ceil(x)
-    output += x
+	a = 1
+	b = 1
+	c = -2*n
+	x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+	x = math.ceil(x)
+	output += x
 elif k == 1:
-    output += n
+	output += n
 
 print(output)
--- Competição/Layers/Chaves/main.py
+++ Competição/Layers/Chaves/main.py
@@ -4,18 +4,18 @@ cstr = ''
 
 n = int(input())
 for i in range(n):
-    cstr += input()
+	cstr += input()
 
-for char in line:
-    if char == '{':
-        counter += 1
-    elif char == '}':
-        counter -= 1
-    if counter < 0:
-        ok = False
-        break
+for char in cstr:
+	if char == '{':
+		counter += 1
+	elif char == '}':
+		counter -= 1
+	if counter < 0:
+		ok = False
+	break
 
 if counter != 0:
-    ok = False
+	ok = False
 
 print("S" if ok else "N")
