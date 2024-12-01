fd69e9c29c652dcd2862ef0ff6ccce020de41ee2
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-12-01 19:11:21 -0300

    Update solution.cpp

--- Treino/3) LeetCode/Graph/2924. Find Champion II/solution.cpp
+++ Treino/3) LeetCode/Graph/2924. Find Champion II/solution.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> count(n, 0);
+        for (int i = 0; i < edges.size(); i++) {
+            count[edges[i][1]]++;
+        }
 
+        int max = -1;
+        for (int i = 0; i < count.size(); i++) {
+            if (count[i] == 0 && max == -1) {
+                max = i;
+                continue;
+            }
+            if (count[i] == 0 && max != -1) {
+                max = -1;
+                break;
+            }
+        }
+
+        return max;
+    }
+};
