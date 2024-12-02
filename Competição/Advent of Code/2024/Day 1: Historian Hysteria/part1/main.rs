9fdc424355af439d0ddc13b1953527354a0b3413
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-12-02 01:20:54 -0300

    Update Advent of Code

---
+++ Competição/Advent of Code/2024/Day 1: Historian Hysteria/part1/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+	let input = "input.txt";
+	let file = File::open(input).expect("Unable to open file");
+	let reader = BufReader::new(file);
+
+	let mut vec1 = Vec::<i32>::new();
+	let mut vec2 = Vec::<i32>::new();
+
+	for line in reader.lines() {
+		let line = line.expect("Unable to read line");
+		let parts = line.split("   ").collect::<Vec<&str>>();
+		let numb1 = parts[0].parse::<i32>().unwrap();
+		let numb2 = parts[1].parse::<i32>().unwrap();
+		vec1.push(numb1);
+		vec2.push(numb2);
+	}
+
+	vec1.sort();
+	vec2.sort();
+
+	if vec1.len() != vec2.len() {
+		panic!("Diferent sizes");
+	}
+
+	let mut dif = 0;
+	for i in 0..vec1.len() {
+		dif += (vec1[i] - vec2[i]).abs();
+	}
+	println!("{}", dif);
+}
---
+++ Competição/Advent of Code/2024/Day 1: Historian Hysteria/part2/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+	let input = "input.txt";
+	let file = File::open(input).expect("Unable to open file");
+	let reader = BufReader::new(file);
+
+	let mut vec1 = Vec::<i32>::new();
+	let mut vec2 = Vec::<i32>::new();
+
+	for line in reader.lines() {
+		let line = line.expect("Unable to read line");
+		let parts = line.split("   ").collect::<Vec<&str>>();
+		let numb1 = parts[0].parse::<i32>().unwrap();
+		let numb2 = parts[1].parse::<i32>().unwrap();
+		vec1.push(numb1);
+		vec2.push(numb2);
+	}
+
+	let mut score = 0;
+	for n1 in &vec1 {
+		let mut repeat = 0;
+		for n2 in &vec2 {
+			repeat += i32::from(n1 == n2);
+		}
+		score += n1 * repeat;
+	}
+	println!("{}", score);
+}
