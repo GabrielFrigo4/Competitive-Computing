33e6e3e138201987a30dc31c354ba2c6d068e154
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-12-05 00:40:08 -0300

    Update Advent of Code

---
+++ Competição/Advent of Code/2024/Day 4 (Ceres Search)/parte 1/main.rs
@@ -0,0 +1,85 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let input = &args[1];
+
+	let file = File::open(input).unwrap();
+	let reader = BufReader::new(file);
+
+	let mut mat: Vec<Vec<char>> = Vec::<Vec<char>>::new();
+	for line in reader.lines() {
+		let line = line.unwrap();
+		mat.push(line.chars().collect());
+	}
+
+	let mut count = 0;
+	for y in 0..mat.len() {
+		for x in 0..mat[y].len() {
+			if x < mat[y].len() - 3 {
+				if mat[y][x] == 'X' &&
+					mat[y][x+1] == 'M' &&
+					mat[y][x+2] == 'A' &&
+					mat[y][x+3] == 'S' {
+					count += 1;
+				}
+				if mat[y][x] == 'S' &&
+					mat[y][x+1] == 'A' &&
+					mat[y][x+2] == 'M' &&
+					mat[y][x+3] == 'X' {
+					count += 1;
+				}
+			}
+
+			if y < mat.len() - 3 {
+				if mat[y][x] == 'X' &&
+					mat[y+1][x] == 'M' &&
+					mat[y+2][x] == 'A' &&
+					mat[y+3][x] == 'S' {
+					count += 1;
+				}
+				if mat[y][x] == 'S' &&
+					mat[y+1][x] == 'A' &&
+					mat[y+2][x] == 'M' &&
+					mat[y+3][x] == 'X' {
+					count += 1;
+				}
+			}
+
+			if y < mat.len() - 3 &&
+				x < mat[y].len() - 3 {
+				if mat[y][x] == 'X' &&
+					mat[y+1][x+1] == 'M' &&
+					mat[y+2][x+2] == 'A' &&
+					mat[y+3][x+3] == 'S' {
+					count += 1;
+				}
+				if mat[y][x] == 'S' &&
+					mat[y+1][x+1] == 'A' &&
+					mat[y+2][x+2] == 'M' &&
+					mat[y+3][x+3] == 'X' {
+					count += 1;
+				}
+			}
+
+			if y < mat.len() - 3 && x > 2 {
+				if mat[y][x] == 'X' &&
+					mat[y+1][x-1] == 'M' &&
+					mat[y+2][x-2] == 'A' &&
+					mat[y+3][x-3] == 'S' {
+					count += 1;
+				}
+					if mat[y][x] == 'S' &&
+					mat[y+1][x-1] == 'A' &&
+					mat[y+2][x-2] == 'M' &&
+					mat[y+3][x-3] == 'X' {
+					count += 1;
+				}
+			}
+		}
+	}
+
+	println!("{}", count);
+}
---
+++ Competição/Advent of Code/2024/Day 4 (Ceres Search)/parte 2/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let input = &args[1];
+
+	let file = File::open(input).unwrap();
+	let reader = BufReader::new(file);
+
+	let mut mat: Vec<Vec<char>> = Vec::<Vec<char>>::new();
+	for line in reader.lines() {
+		let line = line.unwrap();
+		mat.push(line.chars().collect());
+	}
+
+	let mut count = 0;
+	for y in 1..(mat.len()-1) {
+		for x in 1..(mat[y].len()-1) {
+			if mat[y][x] != 'A' {
+				continue;
+			}
+
+			if !(mat[y-1][x-1] == 'M' && mat[y+1][x+1] == 'S') &&
+				!(mat[y-1][x-1] == 'S' && mat[y+1][x+1] == 'M') {
+				continue;
+			}
+
+			if !(mat[y-1][x+1] == 'M' && mat[y+1][x-1] == 'S') &&
+				!(mat[y-1][x+1] == 'S' && mat[y+1][x-1] == 'M') {
+				continue;
+			}
+
+			count += 1;
+		}
+	}
+
+	println!("{}", count);
+}
