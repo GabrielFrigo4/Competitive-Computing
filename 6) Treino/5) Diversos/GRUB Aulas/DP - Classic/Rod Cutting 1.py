1216b3ad523febf314789d0d42da568fbfabbabf
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-09 19:21:53 -0300

    Update DP

---
+++ 6) Treino/5) Diversos/GRUB Aulas/DP - Classic/Rod Cutting 1.py
@@ -0,0 +1,14 @@
+class Solution:
+	def dp(self, price, m):
+		if (self.r[m] >= 0):
+			return self.r[m]
+		self.r[m] = self.r[m - 1]
+		for i in range(m):
+			self.r[m] = max(self.r[m], price[i] + self.dp(price, m - i - 1))
+		return self.r[m]
+
+    def cutRod(self, price):
+		n = len(price)
+		self.r = [-2**60] * (n + 1)
+		self.r[0] = 0
+		return self.dp(price, n)
---
+++ 6) Treino/5) Diversos/GRUB Aulas/DP - Classic/Rod Cutting 2.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+l = int(input())
+n = int(input())
+p = []
+for i in range(n):
+	length, value = [int(j) for j in input().split()]
+	p.append((length, value))
+p.sort()
+	
+r = [-2**60] * (l + 1)
+r[0] = 0
+for i in range(1, l + 1):
+	r[i] = r[i - 1]
+	for length, value in p:
+		if (length > i):
+			break
+		r[i] = max(r[i], value + r[i - length])
+
+print(r[l])
