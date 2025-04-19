e85e4e2ccbd4191f32866e58e41a99a48cb31773
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-19 01:48:53 -0300

    Create Book Shop.py

---
+++ 6) Treino/5) Diversos/GRUB Aulas/DP - Classic/Book Shop.py
@@ -0,0 +1,16 @@
+from array import array
+
+n, x = map(int, input().split())
+h = array('i', list(map(int, input().split())))
+s = array('i', list(map(int, input().split())))
+
+dp = array('i', [0] * (x + 1))
+
+for i in range(n):
+    price, pages = h[i], s[i]
+    for j in range(x, price - 1, -1):
+        new_val = dp[j - price] + pages
+        if new_val > dp[j]:
+            dp[j] = new_val
+
+print(dp[x])
