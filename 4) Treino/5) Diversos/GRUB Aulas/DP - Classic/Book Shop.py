2b1bd1e80045d8b5cb71c5850207f33c5237dee5
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-26 23:30:14 -0300

    Update DP

--- 4) Treino/5) Diversos/GRUB Aulas/DP - Classic/Book Shop.py
+++ 4) Treino/5) Diversos/GRUB Aulas/DP - Classic/Book Shop.py
@@ -7,10 +7,10 @@ s = array('i', list(map(int, input().split())))
 dp = array('i', [0] * (x + 1))
 
 for i in range(n):
-    price, pages = h[i], s[i]
-    for j in range(x, price - 1, -1):
-        new_val = dp[j - price] + pages
-        if new_val > dp[j]:
-            dp[j] = new_val
+	price, pages = h[i], s[i]
+	for j in range(x, price - 1, -1):
+		new_val = dp[j - price] + pages
+		if new_val > dp[j]:
+			dp[j] = new_val
 
 print(dp[x])
---
+++ 4) Treino/5) Diversos/GRUB Aulas/DP - Classic/Brackets in Matrix Chain Multiplication.py
@@ -0,0 +1,30 @@
+class Solution:
+	def rec(self, l, r):
+		if (l == r):
+			self.res += f"{chr(ord("A") + l - 1)}"
+			return self.res
+		self.res += "("
+		self.rec(l, self.sol[l][r])
+		self.rec(self.sol[l][r] + 1, r)
+		self.res += ")"
+		return self.res
+
+	def dp(self, arr, l, r):
+		if (l == r):
+			return 0
+		if (self.memo[l][r] > 0):
+			return self.memo[l][r]
+		self.memo[l][r] = 2 ** 60
+		for i in range(l, r):
+			curr = self.dp(arr, l, i) + self.dp(arr, i + 1, r) + arr[l - 1] * arr[i] * arr[r]
+			if (curr < self.memo[l][r]):
+				self.memo[l][r] = curr
+				self.sol[l][r] = i
+		return self.memo[l][r]
+
+	def matrixChainOrder(self, arr):
+		self.memo = [[0] * len(arr) for i in range(len(arr))]
+		self.sol = [[0] * len(arr) for i in range(len(arr))]
+		self.res = ''
+		self.dp(arr, 1, len(arr) - 1)
+		return self.rec(1, len(arr) - 1)
